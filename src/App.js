388dc721946be99f7bdc07a5096813219d170638
Pravin Gupta <pravingupta1071@gmail.com>
2019-10-23 09:54:56 +0530

    Add router and updates menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+const SectionsPage = (props) => (
+  <div>
+    <h1>{props.match.params.section.toUpperCase()} PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/:section' component={SectionsPage} />
+      </Switch>
     </div>
   );
 }
