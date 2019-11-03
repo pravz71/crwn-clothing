a0af240e202f66d65eed456476ba5de992960106
Pravin Gupta <pravingupta1071@gmail.com>
2019-11-03 11:03:51 +0530

    integrated firebase, created sign in page and added sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyB9GJEpx7q6Rshzk5mvCQ17Kbd-a-l8N9A",
+    authDomain: "crwn-clothing-e7392.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-e7392.firebaseio.com",
+    projectId: "crwn-clothing-e7392",
+    storageBucket: "crwn-clothing-e7392.appspot.com",
+    messagingSenderId: "443624815520",
+    appId: "1:443624815520:web:cbac1d2c32dd145778c2c5",
+    measurementId: "G-H2W1L7VST2"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
