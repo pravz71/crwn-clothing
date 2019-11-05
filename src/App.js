9f21cdcd32671ddb7ac3314c558e7628f6a61b4c
Pravin Gupta <pravingupta1071@gmail.com>
2019-11-05 13:33:50 +0530

    Implemented sign up component and added fuctionality to add authenticated users to database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 import './App.css';
 
@@ -21,8 +21,21 @@ export default class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState(() => ({ currentUser: user}));
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState(() => ({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }));
+          console.log(this.state);
+        });
+      } else {
+        this.setState(() => ({ currentUser: userAuth}));
+      }
     });
   }
 
